5b04c0e7cbdeedc655f98f1b2171dd127563473a
TCorrege <thomas.correge@live.fr>
2017-03-28 08:57:41 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleApplication2/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class homme
+    {
+        public homme(int PID, String PNom, String PPrenom, DateTime Pddn)
+        {
+            ID = PID;
+            Nom = PNom;
+            DDN = Pddn;
+        }
+
+        private int ID;
+        public String Nom;
+        public String Prenom;
+        public DateTime DDN;
+
+
+        public Boolean EstMajeur()
+        {
+            if (DateTime.Today.Year - DDN.Year >= 18)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+    }
+}
